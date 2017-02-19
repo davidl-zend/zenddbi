0e3a4425ff883c5c00f811889f9b3de1fd99875f
David Lowes <david.l@zend.com>
2017-02-19 15:06:26 +0200

    ZD-173 - Split build and packaging scripts

--- pack-zenddbi-71.sh
+++ pack-zenddbi-71.sh
@@ -6,7 +6,7 @@ echo Adding dependent libraries to archive...
 #Extracting library to temporary directory
 cp mariadb-${VERSION}-os400-powerpc.tar.gz /tmp
 pushd /tmp && rm -rf /tmp/mariadb-${VERSION}-os400-powerpc
-tar xzf mariadb-${VERSION}-os400-powerpc.tar.gz && cp /usr/zlocal/zenddbi/mariadb-libdeps/* mariadb-${VERSION}-os400-powerpc/lib
+gtar xzf mariadb-${VERSION}-os400-powerpc.tar.gz && cp /usr/zlocal/zenddbi/mariadb-libdeps/* mariadb-${VERSION}-os400-powerpc/lib
 tar -cf mariadb_i5os_install.tar mariadb-${VERSION}-os400-powerpc/
 echo Packaging mariadb...
 system "CALL PGM(QGPL/MARIADBPCK) PARM('1')"
