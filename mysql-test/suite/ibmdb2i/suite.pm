0b44a41b386d6eb16f3c536b1a8f958bb980161b
David Lowes <david.l@zend.com>
2017-03-30 16:12:05 +0300

    ZD-232 add test cases for ibmdb2i

--- mysql-test/suite/ibmdb2i/suite.pm
+++ mysql-test/suite/ibmdb2i/suite.pm
@@ -2,7 +2,7 @@ package My::Suite::DB2i;
 
 @ISA = qw(My::Suite);
 
-//return "Need DB2i engine" unless defined $::mysqld_variables{'aria-recover'};
+#return "Need DB2i engine" unless defined $::mysqld_variables{'aria-recover'};
 
 bless { };
 
